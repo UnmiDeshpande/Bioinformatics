5c543d39055f6f6c5e78506f0a823afc12587f2e
Unmi <unmideshpande@gmail.com>
2025-09-29 11:12:05 +0400

    Bioinformatics projects
    ROSALIND projects

---
+++ 1.DNA.py
@@ -0,0 +1,9 @@
+s = "ATGCTTCAGAAAGGTCTTACG."
+
+a_count = s.count("A")
+c_count = s.count("C")
+g_count = s.count("G")
+t_count = s.count("T")
+
+
+print(a_count, c_count, g_count, t_count)
---
+++ 4. Rabbits and recorrence.py
@@ -0,0 +1,10 @@
+n, k = 32, 3
+
+F = [0] * (n + 1)
+F[1] = 1
+F[2] = 1
+
+for i in range(3, n + 1):
+    F[i] = F[i-1] + k * F[i-2]
+
+print(F[n])
